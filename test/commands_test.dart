9c702230d947af3db35fb4ff96fc2596e4c853f7
Kristian Urup <kur@easv.dk>
2025-11-25 10:12:42 +0100

    Tests are implemented

--- test/commands_test.dart
+++ test/commands_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:calculator_cli/commands.dart';
 import 'package:calculator_cli/calculator_state.dart';
 import 'package:test/test.dart';
@@ -25,4 +27,75 @@ void main() {
    *   - stack with the last two values divided, and history so that old state
    *   - does nothing when stack length is less than 2
   */
+
+  test('EnterCommand returns a new list with the given number applied', () {
+    final List<num> origStack = [1, 2];
+    var state = CalculatorState(stack: origStack, history: [origStack]);
+    final newState = Enter(10).execute(state);
+
+    expect(newState.stack, equals([1, 2, 10]));
+    expect(newState.stack, isNot(equals(origStack)));
+  });
+
+  test('ClearCommand returns a new CalculatorState with an empty stack', () {
+    final List<num> origStack = [1, 2];
+    CalculatorState state =
+        CalculatorState(stack: origStack, history: [origStack]);
+    final newState = Clear().execute(state);
+
+    expect(newState.stack, equals([]));
+    expect(newState.stack, isNot(equals(origStack)));
+  });
+
+  test(
+      'Undo returns a new CalculatorState with the previous stack and history where the latest item added is removed',
+      () {
+    final List<num> oldStack = [1];
+    final List<num> newStack = [1, 2];
+    CalculatorState state =
+        CalculatorState(stack: newStack, history: [oldStack]);
+    final newState = Undo().execute(state);
+
+    expect(newState.stack, equals([1]));
+    expect(newState.stack, isNot(newStack));
+    expect(newState.history, isEmpty);
+  });
+
+  test('AddCommand returns sum of the last to operands in the stack', () {
+    final origStack = [1, 2];
+    final newStack = Add().operate(origStack[0], origStack[1]);
+
+    expect(newStack, equals(3));
+    expect(newStack, isNot(equals(origStack)));
+  });
+
+  test(
+      'SubstractCommand returns the substraction of the last to operands in the stack',
+      () {
+    final origStack = [1, 2];
+    final newStack = Subtract().operate(origStack[0], origStack[1]);
+
+    expect(newStack, equals(-1));
+    expect(newStack, isNot(equals(origStack)));
+  });
+
+  test(
+      'MultiplyCommand returns the multiplication of the last to operands in the stack',
+      () {
+    final origStack = [1, 2];
+    final newStack = Multiply().operate(origStack[0], origStack[1]);
+
+    expect(newStack, equals(2));
+    expect(newStack, isNot(equals(origStack)));
+  });
+
+  test(
+      'DivideCommand returns the divided value of the last to operands in the stack',
+      () {
+    final origStack = [10, 2];
+    final newStack = Divide().operate(origStack[0], origStack[1]);
+
+    expect(newStack, equals(5));
+    expect(newStack, isNot(equals(origStack)));
+  });
 }
